649c20d12ffd9bc9f5cdc6f8c2786d0c35d42a64
MD-Kayesur <rmdkayesur@gmail.com>
2025-02-22 23:54:32 -0600

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+ 
+
+function Main() {
+    return (
+        <div> 
+          <h2>trdhdfghj</h2>
+            <Outlet></Outlet>
+        </div>
+    )
+}
+
+export default Main
